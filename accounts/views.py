be52548451f24d88411e199d5af6eb9e21541928
chintandavda <davdachintan97@gmail.com>
2024-03-15 18:05:09 +0530

    started user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from .forms import RegistrationForm
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = Account.objects.create_user(username)
+    form = RegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return 

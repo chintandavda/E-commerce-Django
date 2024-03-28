8c413a4582371e614368a91a9f50ae0da377c4a5
chintandavda <davdachintan97@gmail.com>
2024-03-28 17:09:28 +0530

    cart item func

--- carts/views.py
+++ carts/views.py
@@ -4,6 +4,7 @@ from store.models import Product, Variation
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -124,3 +125,26 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item = CartItem.objects.get(product=product, cart=cart,id = cart_item_id)
     cart_item.delete()
     return redirect('cart')
+
+@login_required(login_url='login')
+def checkout(request, total=0, quantity=0, cart_items = None):
+    try:
+        tax, grand_total = 0, 0
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
+    }
+    return render(request, 'store/checkout.html', context)

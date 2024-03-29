711a52ce4868b27b87d888db15e9b33ea68fc478
chintandavda <davdachintan97@gmail.com>
2024-03-29 17:50:38 +0530

    done section17

--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,7 @@ from django.core.mail import EmailMessage
 
 from carts.views import _cart_id
 from carts.models import CartItem, Cart
+import requests
 
 # Create your views here.
 
@@ -67,14 +68,44 @@ def login(request):
                 if is_cart_item_exists:
                     cart_item = CartItem.objects.filter(cart=cart)
 
+                    product_variation = []
                     for item in cart_item:
-                        item.user = user
-                        item.save()
+                        variation = item.variation.all()
+                        product_variation.append(list(variation))
+
+                    # get cart items from user ro access his product_variation
+                    cart_item = CartItem.objects.filter(user = user)
+                    ex_var_list = []
+                    id = []
+                    
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart = cart_item)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                return redirect('dashboard')
+            except:
+                pass
         else:
             messages.error(request, 'invalid logged in')
             return redirect('login')
